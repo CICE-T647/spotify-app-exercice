0a30ac7196e78415b3f76f79012204ed3d85f6b7
meji <soy@meji.es>
2020-01-03 18:54:40 +0100

    Traemos los albums de spotify, token falseado

--- routes/home/index.js
+++ routes/home/index.js
@@ -6,15 +6,17 @@ const token = require("../../spotify-token/getToken")
 
 router.get("/", async (req, res) => {
   try{
+    console.log(token())
     const url = "https://api.spotify.com/v1/browse/new-releases?country=ES"; 
     const response = await axios.get(url, {
       headers: {
-          Authorization : `Bearer ${token()}`,
-          "Content-Type" : "aplication/json"
+          Authorization : "Bearer BQALXINFSWJUpnkK-kvHyWHQ5WVxcF0c_vbS0cZvLuLmHuL8heYOJEaqhJw01X6DF-sX2T5mcgZW2dmtA7UXeUY9gczTdKdTnFb5FkKDi5taoH9EGwmP3snl5UYPvozqK3haL0XmZ7DPPakJ0TQTtTpK17p5nXk1pT25N_58nUM5prRGju-33n7XzQm3DS_zQak26L_Ti2YY1KA5WkLBjPzPeqlFXrkdG96GGEDfATtXoUgtbJwScPGGmx2wky1das7yA5sU", 
+          // Authorization:  `Bearer ${token()}`,
+          "Content-Type" : "aplication/json" 
       }
   });
-    console.log(response.data)
-    res.render("home", {canciones: response.data}, {user});
+    console.log(response.data.albums.items)
+    res.render("home", {albums: response.data.albums.items});
   }catch(error){
     console.log(error);
   }
