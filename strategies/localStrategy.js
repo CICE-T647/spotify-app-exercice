f02c7e231dac582f0e33ae31c2e2ae91743b2229
Alejandro Villar Jimenez <villarjimenez00@gmail.com>
2020-01-18 22:53:34 +0100

    limpiado codigo fantasma y comentarios

--- strategies/localStrategy.js
+++ strategies/localStrategy.js
@@ -4,11 +4,8 @@ const User = require("../models/User");
 
 module.exports = new LocalStrategy(
   {
-    // Los campos usernameField y passwordField, servirán para definir
-    // el nombre de los atributos por los que encontraremos y comprobaremos al usuario
     usernameField: "email",
     passwordField: "password",
-    // Dado que no haremos uso de sesiones, es necesaria especificarlo en las distintas estrategias poniendolo a false.
     session: false
   },
   async (email, password, next) => {
@@ -16,21 +13,15 @@ module.exports = new LocalStrategy(
       `Estrategia local. Información recibida: email ${email}, password${password}`
     );
     try {
-      // Buscamos el usuario a travñes del email
       const user = await User.findOne({ email });
 
-      /*Si no hay usuario enviamos ejecutamos next con el primer parametro (error) a null, el segundo parametro 
-       (data, en este caso usuario) a false y el tercer parametro (info) con el mensaje de error*/
       if (!user) next(null, false, { message: "El usuario no existe" });
 
-      // comprobamos la contraseña y procedemos igual que si no hay usuario
       if (!bcrypt.compareSync(password, user.password))
         next(null, false, { message: "la contraseña no es correcta" });
 
-      // Si el usuario existe y la contraseña es correcta, lo enviamos como segundo parametro de la función next.
       next(null, user);
     } catch (error) {
-      //Si hay un error, lo envíamos como primer parametro de la función next.
       next(error);
     }
   }
