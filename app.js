2f9bedb7f3225cb6aa3cbff6e077dca6060f8230
Alejandro Villar Jimenez <villarjimenez00@gmail.com>
2020-01-18 16:35:45 +0100

    comienzo ejercicio + Interacción 1

--- app.js
+++ app.js
@@ -6,16 +6,21 @@ const app = Express();
 const bodyParser = require("body-parser");
 const SERVER_PORT = process.env.SERVER_PORT || 5000;
 
-const hbs = require("hbs")
+const hbs = require("hbs");
 
-app.use(Express.static(__dirname + "/public"))
-app.set("views", __dirname + "/views")
-app.set("view engine", "hbs")
-hbs.registerPartials(__dirname + '/views/partials');
+app.use(Express.static(__dirname + "/public"));
+app.set("views", __dirname + "/views");
+app.set("view engine", "hbs");
+hbs.registerPartials(__dirname + "/views/partials");
+
+const { signup, login } = require("./routes");
+
+//routes
+app.use("/signup", signup);
+app.use("/login", login);
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
 
 app.listen(SERVER_PORT, () => {
   console.log(`Server listening on port ${SERVER_PORT} `);
